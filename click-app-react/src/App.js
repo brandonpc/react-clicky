e11e677b596eb1909ba70876f1bc4ba62dc0a176
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-07-07 03:12:51 -0600

    most logic working

--- click-app-react/src/App.js
+++ click-app-react/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import pics from "./cards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // sets the state
+  state = {
+    pics,
+    clickedPics: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+  //shuffle the pup cards in the browser when clicked
+  mixItUp = id => {
+    let clickedPics = this.state.clickedPics;
+
+    if (clickedPics.includes(id)) {
+      this.setState({ clickedPics: [], score: 0, status: "Not quite. Try again." });
+      return;
+    } else {
+      clickedPics.push(id)
+
+      if (clickedPics.length === 8) {
+        this.setState({ score: 8, status: "Congratulations! You Won!!", clickedPics: [] });
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ pics, clickedPics, score: clickedPics.length, status: " " });
+
+      for (let i = pics.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [pics[i], pics[j]] = [pics[j], pics[i]];
+      }
+    }
+  }
+
+  // Map over this.state.cards and render a Card component for each card object
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Clicky React App</h1>
+          <p className="App-intro">
+            Here's how it works: you can't click the same picture twice.
+          </p>
+        </header>
+        <Score total={this.state.score}
+          goal={8}
+          status={this.state.status}
+        />
+        <Wrapper>
+          {this.state.pics.map(picture => (
+            <Card
+              mixItUp={this.mixItUp}
+              id={picture.id}
+              key={picture.id}
+              image={picture.image}
+            />
+          ))}
+        </Wrapper>
+
+      </div>
+    );
+  }
 }
 
 export default App;
